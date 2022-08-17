e44b3f5e4cac210555f0fc04212cbfcbe1758306
Tzitzios Ioannis <57442926+ioatzi@users.noreply.github.com>
2022-08-17 12:03:39 +0300

    Update Fill_Level_Bin.ino

--- Arduino Code/Fill_Level_Bin.ino
+++ Arduino Code/Fill_Level_Bin.ino
@@ -1,5 +1,5 @@
 //This example code is in the Public Domain (or CC0 licensed, at your option.)
-//By Evandro Copercini - 2018
+//By Tzitzios Ioannis - 2022
 //
 //This example creates a bridge between Serial and Classical Bluetooth (SPP)
 //and also demonstrate that SerialBT have the same functionalities of a normal Serial
